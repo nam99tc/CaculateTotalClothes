4dfc7ef4465a2d00f1bb32db99965cd6dde13090
luong dinh nam <nam99tc@gmail.com>
2022-12-07 12:37:12 +0700

    Add project files.

---
+++ Caculate/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Caculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private void chooseFIle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file
+            if (file.ShowDialog() == DialogResult.OK) //if there is a file chosen by the user
+            {
+                string fileExt = Path.GetExtension(file.FileName); //get the file extension
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        List<Products> products = new List<Products>();
+                        Type typeOfObject = typeof(Products);
+                        using (IXLWorkbook workbook = new XLWorkbook(file.FileName))
+                        {
+                            var workShaeet = workbook.Worksheets.Select(x => x.Name).ToList();
+                            var workSheet = workbook.Worksheets.Where(x => x.Name == "VNKS (1.11)").First();
+                            var properties = typeOfObject.GetProperties();
+                            var columns = workSheet.FirstRow().Cells().Select((v, i) => new { Value = v.Value, Index = i + 1 });
+
+                            foreach (IXLRow row in workSheet.RowsUsed().Skip(11))
+                            {
+                                Products obj = (Products)Activator.CreateInstance(typeOfObject);
+                                foreach (var prop in properties)
+                                {
+                                    if (prop.Name.ToString() == "Size")
+                                    {
+                                        int colIndexSize = columns.SingleOrDefault(x => x.Value.ToString().ToLower().Contains(prop.Name.ToString().ToLower())).Index;
+                                        Size size = new Size();
+                                        size.XXXS = string.IsNullOrEmpty(row.Cell(colIndexSize).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize).Value);
+                                        size.XXS = string.IsNullOrEmpty(row.Cell(colIndexSize + 1).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 1).Value);
+                                        size.XS = string.IsNullOrEmpty(row.Cell(colIndexSize + 2).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 2).Value);
+                                        size.S = string.IsNullOrEmpty(row.Cell(colIndexSize + 3).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 3).Value);
+                                        size.M = string.IsNullOrEmpty(row.Cell(colIndexSize + 4).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 4).Value);
+                                        size.L = string.IsNullOrEmpty(row.Cell(colIndexSize + 5).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 5).Value);
+                                        size.XL = string.IsNullOrEmpty(row.Cell(colIndexSize + 6).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 6).Value);
+                                        size.XXL = string.IsNullOrEmpty(row.Cell(colIndexSize + 7).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 7).Value);
+                                        size.XXXS = string.IsNullOrEmpty(row.Cell(colIndexSize + 8).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 8).Value);
+                                        size.P = string.IsNullOrEmpty(row.Cell(colIndexSize + 9).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 9).Value);
+                                        size.XXSP = string.IsNullOrEmpty(row.Cell(colIndexSize + 10).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 10).Value);
+                                        size.XSP = string.IsNullOrEmpty(row.Cell(colIndexSize + 11).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 11).Value);
+                                        size.SP = string.IsNullOrEmpty(row.Cell(colIndexSize + 12).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 12).Value);
+                                        size.MP = string.IsNullOrEmpty(row.Cell(colIndexSize + 13).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 13).Value);
+                                        size.LP = string.IsNullOrEmpty(row.Cell(colIndexSize + 14).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 14).Value);
+                                        size.XLP = string.IsNullOrEmpty(row.Cell(colIndexSize + 15).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 15).Value);
+                                        size.XXLP = string.IsNullOrEmpty(row.Cell(colIndexSize + 16).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 16).Value);
+                                        size.XSS = string.IsNullOrEmpty(row.Cell(colIndexSize + 17).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 17).Value);
+                                        size.SM = string.IsNullOrEmpty(row.Cell(colIndexSize + 18).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 18).Value);
+                                        size.ML = string.IsNullOrEmpty(row.Cell(colIndexSize + 19).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 19).Value);
+                                        size.LXL = string.IsNullOrEmpty(row.Cell(colIndexSize + 20).Value.ToString()) ? 0 : Convert.ToInt32(row.Cell(colIndexSize + 20).Value);
+                                        prop.SetValue(obj, size);
+                                    }
+                                    else if (prop.Name.ToString() == "Type")
+                                    {
+                                        int colIndex = columns.SingleOrDefault(x => x.Value.ToString() == "TTL").Index + 1;
+                                        var val = string.IsNullOrEmpty(row.Cell(colIndex).Value.ToString()) ? 1 : Convert.ToInt32(row.Cell(colIndex).Value);
+                                        var type = prop.PropertyType;
+                                        prop.SetValue(obj, Convert.ChangeType(val, type));
+                                    }
+                                    else
+                                    {
+                                        int colIndex = columns.SingleOrDefault(x => x.Value.ToString().ToLower().Contains(prop.Name.ToString().ToLower())).Index;
+                                        var val = row.Cell(colIndex).Value;
+                                        var type = prop.PropertyType;
+                                        prop.SetValue(obj, Convert.ChangeType(val, type));
+                                    }
+                                }
+                                products.Add(obj);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex + "Error");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+        public class Products
+        {
+            public string Buyer { get; set; }
+            public string Carton { get; set; }
+            public string Ctns { get; set; }
+            public string Style { get; set; }
+            public string Color { get; set; }
+            public Size Size { get; set; }
+            public int Type { get; set; }
+        }
+        public class Size
+        {
+            public int XXXS { get; set; } = 0;
+            public int XXS { get; set; } = 0;
+            public int XS { get; set; } = 0;
+            public int S { get; set; } = 0;
+            public int M { get; set; } = 0;
+            public int L { get; set; } = 0;
+            public int XL { get; set; } = 0;
+            public int XXL { get; set; } = 0;
+            public int XXXSP { get; set; } = 0;
+            public int P { get; set; } = 0;
+            public int XXSP { get; set; } = 0;
+            public int XSP { get; set; } = 0;
+            public int SP { get; set; } = 0;
+            public int MP { get; set; } = 0;
+            public int LP { get; set; } = 0;
+            public int XLP { get; set; } = 0;
+            public int XXLP { get; set; } = 0;
+            public int XSS { get; set; } = 0;
+            public int SM { get; set; } = 0;
+            public int ML { get; set; } = 0;
+            public int LXL { get; set; } = 0;
+        }
+    }
+}
---
+++ CaculateTotalClothes/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CaculateTotalClothes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrdView = new System.Windows.Forms.DataGridView();
+            this.chooseFIle = new System.Windows.Forms.Button();
+            this.cbb_styleCode = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_styleCode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrdView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrdView
+            // 
+            this.dataGrdView.AllowUserToAddRows = false;
+            this.dataGrdView.AllowUserToDeleteRows = false;
+            this.dataGrdView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrdView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrdView.Location = new System.Drawing.Point(12, 79);
+            this.dataGrdView.Name = "dataGrdView";
+            this.dataGrdView.ReadOnly = true;
+            this.dataGrdView.RowHeadersWidth = 51;
+            this.dataGrdView.RowTemplate.Height = 29;
+            this.dataGrdView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrdView.Size = new System.Drawing.Size(1645, 698);
+            this.dataGrdView.TabIndex = 0;
+            this.dataGrdView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrdView_CellContentClick);
+            // 
+            // chooseFIle
+            // 
+            this.chooseFIle.Location = new System.Drawing.Point(12, 26);
+            this.chooseFIle.Name = "chooseFIle";
+            this.chooseFIle.Size = new System.Drawing.Size(273, 29);
+            this.chooseFIle.TabIndex = 1;
+            this.chooseFIle.Text = "Choose File ";
+            this.chooseFIle.UseVisualStyleBackColor = true;
+            this.chooseFIle.Click += new System.EventHandler(this.chooseFIle_Click);
+            // 
+            // cbb_styleCode
+            // 
+            this.cbb_styleCode.FormattingEnabled = true;
+            this.cbb_styleCode.Items.AddRange(new object[] {
+            "Buyer",
+            "Style",
+            "Color"});
+            this.cbb_styleCode.Location = new System.Drawing.Point(439, 27);
+            this.cbb_styleCode.Name = "cbb_styleCode";
+            this.cbb_styleCode.Size = new System.Drawing.Size(193, 28);
+            this.cbb_styleCode.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Columns:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(728, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search:";
+            // 
+            // tb_styleCode
+            // 
+            this.tb_styleCode.Location = new System.Drawing.Point(799, 29);
+            this.tb_styleCode.Name = "tb_styleCode";
+            this.tb_styleCode.Size = new System.Drawing.Size(199, 27);
+            this.tb_styleCode.TabIndex = 7;
+            this.tb_styleCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_styleCode_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1669, 789);
+            this.Controls.Add(this.tb_styleCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbb_styleCode);
+            this.Controls.Add(this.chooseFIle);
+            this.Controls.Add(this.dataGrdView);
+            this.Name = "Form1";
+            this.Text = "Caculate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrdView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrdView;
+        private System.Windows.Forms.Button chooseFIle;
+        private System.Windows.Forms.ComboBox cbb_styleCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_styleCode;
+    }
+}
+
